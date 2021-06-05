1431d7aed64aeb159277a19f44e31e25b73131a5
resturr <restur2411@gmail.com>
2021-06-05 10:54:31 +0700

    initial commit

---
+++ app/src/main/java/com/rizki/utspraktik_if1_10118025_rizkirestuillahi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rizki.utspraktik_if1_10118025_rizkirestuillahi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
