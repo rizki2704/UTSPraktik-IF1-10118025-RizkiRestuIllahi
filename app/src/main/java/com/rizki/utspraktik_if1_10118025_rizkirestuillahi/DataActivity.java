4de15bd75e1044d20db213c15b6414ebf9bd8cd2
resturr <restur2411@gmail.com>
2021-06-05 13:28:56 +0700

    beres

--- app/src/main/java/com/rizki/utspraktik_if1_10118025_rizkirestuillahi/DataActivity.java
+++ app/src/main/java/com/rizki/utspraktik_if1_10118025_rizkirestuillahi/DataActivity.java
@@ -1,14 +1,55 @@
 package com.rizki.utspraktik_if1_10118025_rizkirestuillahi;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class DataActivity extends AppCompatActivity {
+    //  5 Juni 2021 10118025 Rizki Restu Illahi IF-1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        Intent intent = getIntent();
+
+        String nik = intent.getStringExtra(MainActivity.Extra_nik);
+        String nama = intent.getStringExtra(MainActivity.Extra_NAMA);
+        String jk = intent.getStringExtra(MainActivity.Extra_jk);
+        String tgl = intent.getStringExtra(MainActivity.Extra_tgl);
+        String hub = intent.getStringExtra(MainActivity.Extra_hub);
+
+        TextView tvNik = findViewById(R.id.isinik);
+        TextView tvNama = findViewById(R.id.isinama);
+        TextView tvTgl = findViewById(R.id.isittl);
+        TextView tvHub = findViewById(R.id.isihubungan);
+        TextView tvJk = findViewById(R.id.isijk);
+
+
+
+        tvNik.setText(nik);
+        tvNama.setText(nama);
+        tvTgl.setText(tgl);
+        tvJk.setText(jk);
+        tvHub.setText(hub);
+
+
+
+    }
+
+    public void simpan(View view) {
+        Intent intent = new Intent(DataActivity.this, DialogueActivity.class);
+        startActivity(intent);
+    }
+
+    public void ubah(View view) {
+        Intent intent = new Intent(DataActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/rizki/utspraktik_if1_10118025_rizkirestuillahi/DialogueActivity.java
+++ app/src/main/java/com/rizki/utspraktik_if1_10118025_rizkirestuillahi/DialogueActivity.java
@@ -2,13 +2,20 @@ package com.rizki.utspraktik_if1_10118025_rizkirestuillahi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DialogueActivity extends AppCompatActivity {
-
+    //  5 Juni 2021 10118025 Rizki Restu Illahi IF-1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialogue);
+    }
+
+    public void ok(View view) {
+        Intent intent = new Intent(DialogueActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
